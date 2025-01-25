883011b6b6ac8e6a2453e94ee163fd4a9e8db99b
Casper Broeren <c.broeren@gmail.com>
2025-01-25 13:39:11 +0100

    Made bg time more clean

--- Sqruffle.Service/BackgroundTimer.cs
+++ Sqruffle.Service/BackgroundTimer.cs
@@ -3,19 +3,13 @@ using Microsoft.Extensions.Hosting;
 using Sqruffle.Domain.General.Events;
 namespace Sqruffle.Service
 {
-    public class BackgroundTimer : BackgroundService
+    public class BackgroundTimer(IBus bus) : BackgroundService
     {
-        private readonly IBus bus;
-
-        public BackgroundTimer(IBus bus)
-        {
-            this.bus = bus;
-        }
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         { 
             while (!stoppingToken.IsCancellationRequested)
             {
-                await bus.Publish(new DailyCheckEvent() {  CurrentTimeUtc = DateTime.UtcNow });
+                await bus.Publish(new DailyCheckEvent() {  CurrentTimeUtc = DateTime.UtcNow }, stoppingToken);
                 await Task.Delay(5000, stoppingToken); // Runs every 5 seconds
             }
         }
